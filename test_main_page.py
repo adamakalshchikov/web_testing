8cdd2d316483b8ae366fc2ee1434a31490608bf6
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-06-30 12:03:30 +0300

    Добавлен переход с главной страницы на Login Page
    test_main_page.py:
     - test_guestcan_go_to_login_page - переход осуществляется
     с посредством объекта класса LoginPage
    
     main_page.py:
      - go_to_login_page - метод возвращает объект LoginPage (страница
      авторизации и регистрации)

--- test_main_page.py
+++ test_main_page.py
@@ -3,9 +3,11 @@ import pytest
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 @pytest.mark.skip(reason="not implemented")
